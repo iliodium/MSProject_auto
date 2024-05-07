ff5aef15df2bfe2aad363e61ca028c38d3e8e9d5
Синякин Даниил <iliodium2003@gmail.com>
2024-05-07 14:56:15 +0300

    ресурсы

--- main.py
+++ main.py
@@ -1,13 +1,11 @@
 import os.path
 import glob
 from collections.abc import Iterable
-from pprint import pprint
 import win32com.client
 
 import openpyxl
 import requests
 from bs4 import BeautifulSoup
-import aspose.tasks as tasks
 
 START_ROW_VEDOM = 5
 END_ROW_VEDOM = 38
@@ -22,18 +20,19 @@ def parce_vedom():
     sheet = file["ВОР"]
 
     name_work = [cell.value.strip() for cell in sheet['B'][START_ROW_VEDOM - 1:END_ROW_VEDOM] if cell.value]
-    GSN = [cell.value.strip() for cell in sheet['E'][START_ROW_VEDOM - 1:END_ROW_VEDOM] if cell.value]
+    GSN = [cell.value.strip().lower().replace('гэсн', '') for cell in sheet['E'][START_ROW_VEDOM - 1:END_ROW_VEDOM]
+           if cell.value]
     volume = [cell.value for cell in sheet['D'][START_ROW_VEDOM - 1:END_ROW_VEDOM] if cell.value]
 
     file.close()
 
-    return [{work: {'gsn': gsn, 'volume': vol}} for work, gsn, vol in zip(name_work, GSN, volume)]
+    return [{work: {'gsn': gsn, 'volume': vol}}
+            for work, gsn, vol in zip(name_work, GSN, volume)]
 
 
 def parce_defsmeta(gsn):
     resources = {g: [] for g in gsn}
     temp_url = r'https://www.defsmeta.com/rgsn/gsn_{}/giesn-{}.php'
-    # https://www.defsmeta.com/rgsn/gsn_11/giesn-11-01-036-01.php
 
     for code in gsn:
         main_code = code.split('-')[0]
@@ -41,23 +40,45 @@ def parce_defsmeta(gsn):
         page = requests.get(url)
         soup = BeautifulSoup(page.text, 'lxml')
 
-        start_element = soup.find('p', string='РАСХОД МАТЕРИАЛОВ')
-        if start_element:
-            elements_between = []
-            current_element = start_element
-            for _ in range(3):
-                elements_between.append(current_element)
-                current_element = current_element.find_next()
+        for label, type_res in zip(
+                ('РАСХОД МАТЕРИАЛОВ', 'ЭКСПЛУАТАЦИЯ МАШИН И МЕХАНИЗМОВ', 'ТРУДОЗАТРАТЫ'),
+                ('material', 'machine', 'people')
+        ):
+            start_element = soup.find('p', string=label)
+            if start_element:
+                elements_between = []
+                current_element = start_element
+                for _ in range(3):
+                    elements_between.append(current_element)
+                    current_element = current_element.find_next()
+                table = elements_between[2]
+                rows = table.find_all('tr')
+                if label == 'ТРУДОЗАТРАТЫ':
+                    rows = rows[1:-len(rows) // 2]
+                else:
+                    rows = rows[1:-1]
 
-            table = elements_between[2]
-            rows = table.find_all('tr')[1:-1]
+                for material in rows:
+                    material = material.find_all('td')
+                    if label == 'ТРУДОЗАТРАТЫ':
+                        material = material[1:]
+                        consumption = float(material[2].text.replace(',', '.'))
+                        unit = material[1].text
+                        name = material[0].text
+                    else:
+                        material = material[1:-1]
+                        consumption = float(material[3].text.replace(',', '.'))
+                        unit = material[2].text
+                        name = material[1].text
 
-            for material in rows:
-                material = material.find_all('td')[1:-1]
-                resources[code].append({
-                    'name': material[1].text,
-                    'consumption': float(material[3].text.replace(',', '.')),
-                })
+                    resources[code].append({
+                        'name': name,
+                        'consumption': consumption,
+                        'type': type_res,
+                        'unit': unit.replace('\xa0', '')
+                    })
+                    if resources[code][-1]['unit'] == 'м':
+                        resources[code][-1]['unit'] = 'метры'
 
     return resources
 
@@ -105,20 +126,18 @@ class MSProject:
         self._save_file()
         self._close_file()
 
+from openpyxl import Workbook
 
 def main():
     project = MSProject(glob.glob(os.path.join(DIRECTORY, '*.mpp'))[0])
-    vedom = parce_vedom()[:]
+    vedom = parce_vedom()[:6]
 
     gsns = []
     for w in vedom:
         for g in w.values():
             gsns.append(g['gsn'])
 
-    resources_defsmeta = parce_defsmeta(gsns)
-
-    # pprint(vedom)
-    # pprint(resources_defsmeta)
+    resources_defsmeta = parce_defsmeta(set(gsns))
 
     # Добавление всех ресурсов в проект
     set_resources = set()
@@ -133,25 +152,53 @@ def main():
     tasks_obj = project.get_tasks_object()
     list_tasks_obj = list(tasks_obj)
     resources = project.get_resources_name_id()
-
-    for task_vedom in vedom:
+    count_vedom = len(vedom)
+    new_vedom = [['Работа', 'ГЭСН', 'Материал', 'Объем']]
+    for ind, task_vedom in enumerate(vedom, start=1):
+        new_vedom.append([])
+        print(f'{ind}/{count_vedom}')
         for task_obj in list_tasks_obj:
             name_task_vedom = list(task_vedom.keys())[0].strip()
             if task_obj.Name.strip() == name_task_vedom:
                 gsn = task_vedom[name_task_vedom]['gsn']
                 gsn_def = resources_defsmeta[gsn]
                 if not list(task_obj.Assignments):
+                    new_vedom[-1].append(name_task_vedom)
+                    new_vedom[-1].append(gsn)
+                    new_vedom[-1].append(None)
+                    new_vedom[-1].append(None)
                     for res_name in gsn_def:
-
+                        new_vedom.append([None, None])
+                        new_vedom[-1].append(res_name['name'])
+                        new_vedom[-1].append(res_name['consumption'] * task_vedom[name_task_vedom]['volume'])
+                        continue
                         res_id = resources[res_name['name']]
                         res_obj = resources_obj.Item(res_id)
-                        res_obj.Code = "м³"
-                        res_obj.MaxUnits = res_name['consumption']
+
+                        if res_name['type'] == 'material':
+                            res_obj.Type = 1
+                            res_obj.MaterialLabel = res_name['unit']
+                        elif res_name['type'] == 'machine':
+                            res_obj.Group = "Машины"
+
+                        elif res_name['type'] == 'people':
+                            res_obj.Group = "Рабочие"
+
                         try:
-                            task_obj.Assignments.Add(task_obj.ID, res_id)
+                            task_obj.FixedDuration = True
+                            t = task_obj.Assignments.Add(task_obj.ID, res_id)
+                            t.Units = res_name['consumption'] * task_vedom[name_task_vedom]['volume']
+
                         except Exception as e:
                             print(e)
+                    project._save_file()
                     break
+
+    wb = Workbook()
+    ws1 = wb.create_sheet("Материалы")
+    for row in new_vedom:
+        ws1.append(row)
+    wb.save('Материалы.xlsx')
 
     project.close()
 
